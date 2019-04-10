8b9f9921fc3ef47ba909801ae4703224f298cc46
Sammy <1014268@student.sae.edu.au>
2019-04-10 11:31:49 +0800

    Update_10/04/19
    Loaded PIano Sprites
    Notes now spawn and move

---
+++ Assets/Scripts/NoteLogic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteLogic : MonoBehaviour
+{
+    private NoteSpawn noteSpawn;
+    public float speed = 10.0f;
+    private Rigidbody2D rb;
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(-speed, 0);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (noteSpawn.spawnWhere>5 && noteSpawn.spawnWhere<11)
+        {
+            SpriteRenderer.flipX;
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/NoteSpawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteSpawn : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] notePrefab;
+    public GameObject[] noteClone;
+    public int spawnWhere = 0;
+    public float spawnTime = 1.0f;
+
+    void spawnNote()
+    {
+        spawnWhere = Random.Range(0, 22);
+        noteClone[0] = Instantiate(notePrefab[0], spawnPoints[spawnWhere].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+    }
+
+    IEnumerator spawnRate()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnTime);
+            spawnNote();
+        }
+        
+    }
+
+    void Start()
+    {
+        StartCoroutine(spawnRate());
+    }
+}
